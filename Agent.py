49022cf3b783dd468922e8b8d16d202d28f58482
Brian Maher <bmaher91@gmail.com>
2016-11-16 09:43:10 -0300

    use old strategy when we don't know what to do, matches (csi) D:7/0/5, E:10/0/2

--- Agent.py
+++ Agent.py
@@ -5,8 +5,10 @@ from ProblemClasses.Intersection import Intersection
 from ProblemClasses.Disjunction import Disjunction
 from ProblemClasses.PixelSubtraction import PixelSubtraction
 from ProblemClasses.PixelAddition import PixelAddition
+from Transform import Transform
 from PIL import Image
 from ImageUtils import ImageUtils
+import math
 
 
 class Agent:
@@ -18,6 +20,37 @@ class Agent:
         self.problem = None
         self.problem_figures = {}
 
+    @staticmethod
+    def generate_problem_images(problem):
+        """Generate images for each solution and problem figure.
+
+        Args:
+            problem (RavensProblem): A object containing all information about
+                the RPM problem being solved.
+
+            Returns:
+                (dict): Dictionary of problem figures.
+                (dict): Dictionary of solution figures.
+                    Both dictionary returns respond with a dictionary object
+                    in the form of:
+                        {
+                            'image':  # The figure image as a PIL image.
+                            'pixels':  # A list of pixels in the image.
+                        }
+        """
+
+        problem_figures = {}
+        solution_figures = {}
+        for figure in problem.figures.itervalues():
+            figure_image = Image.open(figure.visualFilename)
+            image_pixels = list(figure_image.getdata())
+            image_details = dict(image=figure_image, pixels=image_pixels)
+            if figure.name in Base.PROBLEM_FIGURE_KEYS:
+                problem_figures[figure.name] = image_details
+            elif figure.name in Base.SOLUTION_FIGURE_KEYS:
+                solution_figures[figure.name] = image_details
+        return problem_figures, solution_figures
+
     def Solve(self, problem):
         self.problem = problem
 
@@ -28,7 +61,7 @@ class Agent:
         problem_class = self.__classify_problem()
 
         if problem_class is None:
-            return Base.SKIP
+            return self.transformation_match_percentage_strategy()
 
         problem_solver = problem_class(self.problem_figures)
 
@@ -48,3 +81,37 @@ class Agent:
             image = Image.open(figure.visualFilename)
             image_data = ImageUtils.get_image_data(image)
             self.problem_figures[figure.name] = image_data
+
+    def transformation_match_percentage_strategy(self):
+        solution_map = {'AC': 'G', 'DF': 'G', 'AG': 'C', 'BH': 'C'}
+        problem_figures, solution_figures = \
+            Agent.generate_problem_images(self.problem)
+
+        closest_match = {'name': '-1', 'difference': 2.00}
+
+        # See solution map.
+        # Example: 2x2 matrix
+        # from_pair: 'AB'
+        # apply_to: 'C'
+        for from_pair, apply_to in solution_map.iteritems():
+            # Find the match between all transformations between the two images.
+            image1_data = problem_figures[from_pair[0]]
+            image2_data = problem_figures[from_pair[1]]
+            images_transformations = \
+                Transform.generate_transforms_data(image1_data, image2_data)
+            apply_to_image_data = problem_figures[apply_to]
+            # Loop over each solution image.
+            for solution_name, solution_image_data in \
+                    solution_figures.iteritems():
+                # Loop over each transformation and see if the transformations
+                # match up.
+                for transformation in images_transformations.itervalues():
+                    apply_to_image_match = transformation.apply_and_compare(
+                        apply_to_image_data, solution_image_data)
+                    match_difference = \
+                        math.fabs(apply_to_image_match - transformation.match)
+                    if match_difference < closest_match['difference']:
+                        closest_match['name'] = solution_name
+                        closest_match['difference'] = match_difference
+
+        return int(closest_match['name'])
--- ImageUtils.py
+++ ImageUtils.py
@@ -16,5 +16,35 @@ class ImageUtils:
 
         return np.int_(image_data)
 
+    @staticmethod
+    def match_percentage(image1_pixels, image2_pixels):
+        """Compares two image's pixels and returns the percentage that those
+        two pixel lists match.
+
+        Args:
+            image1_pixels (list): A list of pixels in an image.
+            image2_pixels (list): A list of pixels in an image.
+
+        Return:
+            (float): Percentage the two pixel lists match.
+        """
+
+        match, total = 0, 0
+        for i in range(len(image1_pixels)):
+            if image1_pixels[i] == image2_pixels[i]:
+                match += 1
+                total += 1
+            else:
+                total += 1
+        return float(match) / float(total)
+
+    @staticmethod
+    def black_pixel_count(image_pixels):
+        black = 0
+        for i in range(len(image_pixels)):
+            if image_pixels[i] == (0, 0, 0, 255):
+                black += 1
+        return black
+
     def __init__(self):
         pass
